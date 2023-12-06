1a9506390561fe545a924c513a8c5fb9ec790390
JJCO10 <juanjosecarmonaortiz@gmail.com>
2023-12-06 14:48:25 -0500

    conexion a la base de datos

--- DAL/BaseDatos.cs
+++ DAL/BaseDatos.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DAL
 {
-    internal class BaseDatos
+    public class BaseDatos
     {
+        protected SqlConnection conexion;
+        protected string cadenaConexion = "Server=.\\SQLEXPRESS;Database=BD_BOLIS;Trusted_Connection=True;";
+        public BaseDatos()
+        {
+            conexion = new SqlConnection(cadenaConexion);
+        }
+        public string AbrirConexion()
+        {
+            conexion.Open();
+            return conexion.State.ToString();
+        }
+
+        public string CerrarConexion()
+        {
+            conexion.Close();
+            return conexion.State.ToString();
+        }
     }
 }
