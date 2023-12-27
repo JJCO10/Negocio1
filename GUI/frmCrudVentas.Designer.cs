b3d508fb30aad39c99060a2dfdd0da4ddd5258c1
JJCO10 <juanjosecarmonaortiz@gmail.com>
2023-12-27 10:46:36 -0500

    actualizacion GUI validacion de textbox

--- GUI/frmCrudVentas.Designer.cs
+++ GUI/frmCrudVentas.Designer.cs
@@ -29,6 +29,16 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCantidadBoli = new System.Windows.Forms.TextBox();
+            this.txtPrecioTotalBoli = new System.Windows.Forms.TextBox();
+            this.txtSaborBoli = new System.Windows.Forms.TextBox();
+            this.txtIdBoli = new System.Windows.Forms.TextBox();
+            this.dtpFechaVenta = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.btnInsertarVenta = new System.Windows.Forms.Button();
@@ -36,16 +46,6 @@
             this.btnEliminarVenta = new System.Windows.Forms.Button();
             this.btnVolverMenu = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.dtpFechaVenta = new System.Windows.Forms.DateTimePicker();
-            this.txtIdBoli = new System.Windows.Forms.TextBox();
-            this.txtSaborBoli = new System.Windows.Forms.TextBox();
-            this.txtPrecioTotalBoli = new System.Windows.Forms.TextBox();
-            this.txtCantidadBoli = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -76,6 +76,104 @@
             this.groupBox1.Size = new System.Drawing.Size(972, 527);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
+            // 
+            // txtCantidadBoli
+            // 
+            this.txtCantidadBoli.Location = new System.Drawing.Point(269, 386);
+            this.txtCantidadBoli.Name = "txtCantidadBoli";
+            this.txtCantidadBoli.Size = new System.Drawing.Size(200, 20);
+            this.txtCantidadBoli.TabIndex = 17;
+            this.txtCantidadBoli.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCantidadBoli_KeyPress);
+            // 
+            // txtPrecioTotalBoli
+            // 
+            this.txtPrecioTotalBoli.Location = new System.Drawing.Point(269, 486);
+            this.txtPrecioTotalBoli.Name = "txtPrecioTotalBoli";
+            this.txtPrecioTotalBoli.Size = new System.Drawing.Size(200, 20);
+            this.txtPrecioTotalBoli.TabIndex = 16;
+            // 
+            // txtSaborBoli
+            // 
+            this.txtSaborBoli.Location = new System.Drawing.Point(269, 279);
+            this.txtSaborBoli.Name = "txtSaborBoli";
+            this.txtSaborBoli.Size = new System.Drawing.Size(200, 20);
+            this.txtSaborBoli.TabIndex = 15;
+            this.txtSaborBoli.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSaborBoli_KeyPress);
+            // 
+            // txtIdBoli
+            // 
+            this.txtIdBoli.Location = new System.Drawing.Point(269, 73);
+            this.txtIdBoli.Name = "txtIdBoli";
+            this.txtIdBoli.Size = new System.Drawing.Size(200, 20);
+            this.txtIdBoli.TabIndex = 14;
+            // 
+            // dtpFechaVenta
+            // 
+            this.dtpFechaVenta.Location = new System.Drawing.Point(269, 172);
+            this.dtpFechaVenta.Name = "dtpFechaVenta";
+            this.dtpFechaVenta.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaVenta.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Cyan;
+            this.label5.Location = new System.Drawing.Point(264, 442);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 29);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Precio Total";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Cyan;
+            this.label4.Location = new System.Drawing.Point(264, 333);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(220, 29);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Cantidad de Bolis";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Cyan;
+            this.label3.Location = new System.Drawing.Point(264, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 29);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Sabor del Boli";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Cyan;
+            this.label2.Location = new System.Drawing.Point(264, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 29);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Fecha de Venta";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cyan;
+            this.label1.Location = new System.Drawing.Point(264, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Codigo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // pictureBox2
             // 
@@ -172,102 +270,6 @@
             this.pictureBox1.Size = new System.Drawing.Size(226, 520);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Cyan;
-            this.label1.Location = new System.Drawing.Point(264, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 29);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Codigo";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Cyan;
-            this.label2.Location = new System.Drawing.Point(264, 120);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(195, 29);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Fecha de Venta";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Cyan;
-            this.label3.Location = new System.Drawing.Point(264, 228);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(180, 29);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Sabor del Boli";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Cyan;
-            this.label4.Location = new System.Drawing.Point(264, 333);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(220, 29);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Cantidad de Bolis";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.Cyan;
-            this.label5.Location = new System.Drawing.Point(264, 442);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(156, 29);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "Precio Total";
-            // 
-            // dtpFechaVenta
-            // 
-            this.dtpFechaVenta.Location = new System.Drawing.Point(269, 172);
-            this.dtpFechaVenta.Name = "dtpFechaVenta";
-            this.dtpFechaVenta.Size = new System.Drawing.Size(200, 20);
-            this.dtpFechaVenta.TabIndex = 13;
-            // 
-            // txtIdBoli
-            // 
-            this.txtIdBoli.Location = new System.Drawing.Point(269, 73);
-            this.txtIdBoli.Name = "txtIdBoli";
-            this.txtIdBoli.Size = new System.Drawing.Size(200, 20);
-            this.txtIdBoli.TabIndex = 14;
-            // 
-            // txtSaborBoli
-            // 
-            this.txtSaborBoli.Location = new System.Drawing.Point(269, 279);
-            this.txtSaborBoli.Name = "txtSaborBoli";
-            this.txtSaborBoli.Size = new System.Drawing.Size(200, 20);
-            this.txtSaborBoli.TabIndex = 15;
-            // 
-            // txtPrecioTotalBoli
-            // 
-            this.txtPrecioTotalBoli.Location = new System.Drawing.Point(269, 486);
-            this.txtPrecioTotalBoli.Name = "txtPrecioTotalBoli";
-            this.txtPrecioTotalBoli.Size = new System.Drawing.Size(200, 20);
-            this.txtPrecioTotalBoli.TabIndex = 16;
-            // 
-            // txtCantidadBoli
-            // 
-            this.txtCantidadBoli.Location = new System.Drawing.Point(269, 386);
-            this.txtCantidadBoli.Name = "txtCantidadBoli";
-            this.txtCantidadBoli.Size = new System.Drawing.Size(200, 20);
-            this.txtCantidadBoli.TabIndex = 17;
             // 
             // frmCrudVentas
             // 
--- GUI/frmCrudVentas.cs
+++ GUI/frmCrudVentas.cs
@@ -28,5 +28,23 @@ namespace GUI
         {
 
         }
+
+        private void txtCantidadBoli_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txtSaborBoli_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
+            {
+                e.Handled = true;
+                return;
+            }
+        }
     }
 }
